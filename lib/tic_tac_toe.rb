861998ae87ef7998fd067fe938c62dfc0e2b3516
Evan <evanfujita@gmail.com>
2020-12-16 21:18:45 -0600

    commit tree a48a4dcd3d42753fd03ceec9f88e026f21fd8148

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class TicTacToe
+
+    attr_accessor :board
+
+    def initialize
+        @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
+    end
+
+    def display_board(board)
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "---|---|---"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "---|---|---"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+        puts "---|---|---"
+    end
+
+    
+    WIN_COMBINATIONS = [
+        [0, 1, 2], #top row 
+        [3, 4, 5], #middle row
+        [6, 7, 8], #bottom row
+        [0, 3, 6], #left column
+        [1, 4, 7], #middle column 
+        [2, 5, 8], #right column
+        [0, 4, 8], #left-to-right diagonal
+        [2, 4, 6]  #right-to-left diagonal
+    ]
+
+
+
+
+#a = TicTacToe.new
+#binding.pry
+
+end
